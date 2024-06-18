dd26f6edc3232c259f5d46900630bc597bfc16ee
Emads <emadsaab722@gmail.com>
2024-06-18 14:28:11 +0300

    create menu in Home webpage class

--- webpages/home.py
+++ webpages/home.py
@@ -15,13 +15,32 @@ class Home:
         toolbar = jp.QToolbar(a=header)  # Add a toolbar to the header
 
         # Create a drawer (sidebar) for the layout
-        the_drawer = jp.QDrawer(
+        left_drawer = jp.QDrawer(
             a=layout, show_if_above=True, v_model="leftDrawerOpen", bordered=True)
+
+        # Add a scroll area to the drawer
+        # Scrollable area to hold the list of links
+        scroller = jp.QScrollArea(a=left_drawer, classes="fit")
+
+        # Create a list within the scroll area
+        qlist = jp.QList(a=scroller)  # List container for navigation links
+
+        # Define the CSS classes for the anchor tags
+        a_classes = "m-2 p-2 text-lg text-blue-400 hover:text-blue-700"
+
+        # Add anchor tags (links) to the list
+        jp.A(a=qlist, href="/", text="Home", classes=a_classes)  # Home link
+        jp.Br(a=qlist)  # Line break for spacing
+        jp.A(a=qlist, href="/dictionary", text="Dictionary",
+             classes=a_classes)  # Dictionary link
+        jp.Br(a=qlist)  # Line break for spacing
+        jp.A(a=qlist, href="/about", text="About",
+             classes=a_classes)  # About link
 
         # Create a button in the toolbar to toggle the drawer
         toggle_btn = jp.QBtn(a=toolbar, dense=True, flat=True,
                              round=True, icon="menu", click=cls.move_drawer)
-        toggle_btn.drawer = the_drawer  # Attach the drawer to the button for easy access
+        toggle_btn.drawer = left_drawer  # Attach the drawer to the button for easy access
 
         # Add a title to the toolbar
         jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
